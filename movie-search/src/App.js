58e6f4722df66254e54f017d4780964b35bd0a0b
lucasromerodb <lucasromerodb@gmail.com>
2018-06-03 01:40:41 -0300

    estilos y listado de peliculas al ejecutar una búsqueda

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -6,17 +6,43 @@ import 'bulma/css/bulma.css';
 import './App.css';
 
 class App extends Component {
+
+  state = { movieResults: [] }
+
+  _handleResults = results => {
+    this.setState({ movieResults: results })
+  }
+
+  _renderResults = () => {
+    const { movieResults } = this.state
+    return movieResults.map(movie => {
+      return <p>{movie.Title}</p>
+    })
+  }
+
   render() {
     return (
-      <section className="hero is-danger is-bold">
-        <div className="hero-body">
-          <div className="container container__lg">
-            <Title>Movie Searcher</Title>
-            <SearchForm />
+      <div>
+        <section className="hero is-bold">
+          <div className="hero-body">
+            <div className="container container__lg">
+              <Title>Movie Searcher</Title>
+              <SearchForm onResults={this._handleResults}/>
+              {
+                this.state.movieResults.length < 1
+                ? <h4 className='is-size-6' style={{marginTop: 10, marginBottom: 10}}>Without results.</h4>
+                : this._renderResults()
+              }
+            </div>
           </div>
+        </section>
+        <div className='container container__lg has-text-centered'>
+          <a href="https://bulma.io" className='middle-align'>
+            <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="128" height="24"/>
+          </a>
         </div>
-      </section>
-    );
+      </div>
+    )
   }
 }
 
