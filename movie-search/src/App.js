6670c5a500caa7ecf73479927a71ae80615f8fb8
lucasromerodb <lucasromerodb@gmail.com>
2018-06-03 00:08:55 -0300

    estilos varios

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
+
+import 'bulma/css/bulma.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section className="hero is-danger is-bold">
+        <div className="hero-body">
+          <div className="container container__lg">
+            <Title>Movie Searcher</Title>
+            <SearchForm />
+          </div>
+        </div>
+      </section>
     );
   }
 }
