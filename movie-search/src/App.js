d27b5eb33324907f49ebbe68d1b2144625f709b3
lucasromerodb <lucasromerodb@gmail.com>
2018-06-04 21:59:04 -0300

    separo la home y detail de app

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
-import { Title } from "./components/Title"
-import { SearchForm } from "./components/SearchForm"
-import { MoviesList } from "./components/MoviesList"
+
+import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 
 import "bulma/css/bulma.css"
@@ -9,53 +8,19 @@ import "./App.css"
 
 class App extends Component {
 
-  state = { movieResults: [], userSearch: false }
-
-  _handleResults = results => {
-    this.setState({ movieResults: results, userSearch: true })
-  }
-
-  _renderResults() {
-    return this.state.movieResults.length === 0
-    ? <h6 className="is-size-6 form__text"> <span role="img" aria-label="sad face">😞</span>  Sorry, try something different...</h6>
-    : <MoviesList movieResults={this.state.movieResults} />
-  }
-
   render() {
 
     const url = new URL(document.location)
-    const hasId = url.searchParams.has('id')
-
-    if (hasId) {
-      return <Detail id={url.searchParams.get('id')} />
-    }
+    const Page = url.searchParams.has('id')
+      ? <Detail id={url.searchParams.get('id')} />
+      : <Home/>
 
     return (
       <div className="App">
-        <section className="hero is-bold">
-          <div className="hero-body">
-            <div className="container container__lg">
-              <Title>Entertainment Searcher</Title>
-              <SearchForm onResults={this._handleResults}/>
-            </div>
-            <div className="container">
-              {
-                this.state.userSearch
-                ? this._renderResults()
-                : <h4 className="is-size-6 form__text"> You can search Movies, Series or Videogames </h4>
-              }
-            </div>
-            <hr/>
-            <div className="container container__lg">
-              <p>I use <a href="https://www.omdbapi.com/" target="_blank" rel="noopener noreferrer">OMDB API</a> for results</p>
-              <p><i>
-                Follow me on <a href="https://twitter.com/lucasromerodb" target="_blank" rel="noopener noreferrer">Twitter</a> or check my <a href="https://github.com/lucasromerodb" target="_blank" rel="noopener noreferrer">Github</a>
-              </i></p>
-            </div>
-          </div>
-        </section>
+        {Page}
       </div>
     )
+
   }
 }
 
---
+++ movie-search/src/pages/Home.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { Title } from "../components/Title"
+import { SearchForm } from "../components/SearchForm"
+import { MoviesList } from "../components/MoviesList"
+
+export class Home extends Component {
+
+  state = { movieResults: [], userSearch: false }
+
+  _handleResults = results => {
+    this.setState({ movieResults: results, userSearch: true })
+  }
+
+  _renderResults() {
+    return this.state.movieResults.length === 0
+    ? <h6 className="is-size-6 form__text"> <span role="img" aria-label="sad face">😞</span>  Sorry, try something different...</h6>
+    : <MoviesList movieResults={this.state.movieResults} />
+  }
+
+  render() {
+    return (
+      <div>
+        <section className="hero is-bold">
+          <div className="hero-body">
+            <div className="container container__lg">
+              <Title>Entertainment Searcher</Title>
+              <SearchForm onResults={this._handleResults}/>
+            </div>
+            <div className="container">
+              {
+                this.state.userSearch
+                ? this._renderResults()
+                : <h4 className="is-size-6 form__text"> You can search Movies, Series or Videogames </h4>
+              }
+            </div>
+            <hr/>
+            <div className="container container__lg">
+              <p>I use <a href="https://www.omdbapi.com/" target="_blank" rel="noopener noreferrer">OMDB API</a> for results</p>
+              <p><i>
+                Follow me on <a href="https://twitter.com/lucasromerodb" target="_blank" rel="noopener noreferrer">Twitter</a> or check my <a href="https://github.com/lucasromerodb" target="_blank" rel="noopener noreferrer">Github</a>
+              </i></p>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
